94bcd2051d81aaf1b8721add09b60c1af4bc2bd1
lypane <hochetlouan7@gmail.com>
2026-03-02 17:27:50 +0100

    affichage d'une batterie

--- app/src/main/java/iut/projet/hochet/ui/device/DeviceFragment.java
+++ app/src/main/java/iut/projet/hochet/ui/device/DeviceFragment.java
@@ -25,6 +25,8 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import androidx.lifecycle.Observer;
 
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+
 public class DeviceFragment extends Fragment implements Observer {
 
     private FragmentDeviceBinding binding;
@@ -33,7 +35,12 @@ public class DeviceFragment extends Fragment implements Observer {
 
     private String device = "not connected";
     private String mode = "ouais";
-    // ...
+    private String etat = "";
+    private String dischargeLimit = "";
+    private String chargeRate = "";
+    private String capacity = "";
+    private String level = "";
+    private CircularProgressIndicator progress;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -45,6 +52,11 @@ public class DeviceFragment extends Fragment implements Observer {
 
         composants.put("mode", binding.mode);
         composants.put("level", binding.level);
+        composants.put("Capacity", binding.Capacity);
+        composants.put("ChargeRate", binding.ChargeRate);
+        composants.put("DischargeLimit", binding.DischargeLimit);
+        composants.put("etat", binding.etat);
+        progress = binding.circularProgress;
         // ...
 
         Log.d("DeviceFragment", "registering observer");
@@ -62,6 +74,12 @@ public class DeviceFragment extends Fragment implements Observer {
         // ...
 
         ((TextView) composants.get("mode")).setText("mode : " + mode);
+        ((TextView) composants.get("etat")).setText(etat);
+        ((TextView) composants.get("DischargeLimit")).setText(dischargeLimit);
+        ((TextView) composants.get("Capacity")).setText(capacity);
+        ((TextView) composants.get("ChargeRate")).setText(chargeRate);
+        ((TextView) composants.get("level")).setText(level);
+
 
     }
 
@@ -78,6 +96,26 @@ public class DeviceFragment extends Fragment implements Observer {
                 JSONObject jsonDeviceConfig = datas.getJSONObject("config");
                 if (!jsonDeviceConfig.isNull("operationMode"))
                     mode = jsonDeviceConfig.getString("operationMode");
+                JSONObject jsonDeviceChargeState = datas.getJSONObject("chargeState");
+                if (!jsonDeviceChargeState.isNull("status"))
+                    etat = jsonDeviceChargeState.getString("status");
+                if (!jsonDeviceChargeState.isNull("chargeRate"))
+                    chargeRate = jsonDeviceChargeState.getString("chargeRate");
+                if (!jsonDeviceChargeState.isNull("dischargeLimit"))
+                    dischargeLimit = jsonDeviceChargeState.getString("dischargeLimit");
+                if (!jsonDeviceChargeState.isNull("batteryLevel"))
+                    level = jsonDeviceChargeState.getString("batteryLevel");
+                if (!jsonDeviceChargeState.isNull("batteryCapacity"))
+                    capacity = jsonDeviceChargeState.getString("batteryCapacity");
+                int batteryPercent = Integer.parseInt(level);
+                progress.setProgress(batteryPercent);
+                if (batteryPercent <= 20) {
+                    progress.setIndicatorColor(Color.RED);
+                } else if (batteryPercent <= 50) {
+                    progress.setIndicatorColor(Color.YELLOW);
+                } else {
+                    progress.setIndicatorColor(Color.GREEN);
+                }
                 // device state
                 // ...
 
